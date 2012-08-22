6af111c09fef537d4ac3f6cb2eabd7480567032c
Dominic Bartl <domiii.1992@gmail.com>
2012-08-22 22:41:27 +0200

    Hint on password fields on non https sites

--- src/privasee.js
+++ src/privasee.js
@@ -1,1 +1,17 @@
-//HI
+
+$(document).ready(function(){
+	if(window.location.protocol == "http:"){
+		$("input[type='password']").focus(onFieldFocus);
+		$("input[type='password']").blur(onFieldFBlur);
+	}
+});
+
+function onFieldFocus(event){
+	$(event.target).after("<div class='pw-hint'><h2>Warning!</h2>You're entering a password on a site which doesn't use HTTPS(<a href='#'>?</a>). This isn't secure!</div>");
+}
+
+function onFieldFBlur(event){
+	$(".pw-hint").remove();
+}	
+
+
