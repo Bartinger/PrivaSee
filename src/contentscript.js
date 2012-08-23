2390d901fd0c3cc1756dafd13b8f96c614cf7cff
Thomas Taschauer <tomtasche@gmail.com>
2012-08-23 14:36:31 +0200

    add information page about https

--- src/contentscript.js
+++ src/contentscript.js
@@ -3,8 +3,7 @@ function toggleBlock(enabled) {
 }
 
 function onFieldFocus(event){
-	$(event.target).after("<div class='pw-hint'><h2>Warning!</h2>You're entering a password on a site which doesn't use HTTPS(<a href='#'>?</a>). This isn't secure!</div>");
-	chrome.pageAction.setIcon("icons/16-good.png");
+	$(event.target).after("<div class='pw-hint'><h2>Warning!</h2>You're entering a password on a site which isn't secured by HTTPS (<a href='chrome-extension://ojbegobhciacikcafiomhinpcjhibkgg/src/about-https.html'>?</a>). Others might be able to see your password!</div>");
 }
 
 function onFieldFBlur(event){
