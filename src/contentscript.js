a84ab129b67eb3904a54061f58b9addce831ec3f
Dominic Bartl <domiii.1992@gmail.com>
2012-08-23 15:30:37 +0200

    What's HTTPS link

--- src/contentscript.js
+++ src/contentscript.js
@@ -1,13 +1,36 @@
+var over = false;
+var focused = false;
+
 function toggleBlock(enabled) {
    chrome.extension.sendMessage({enabled: enabled});
 }
 
 function onFieldFocus(event){
-	$(event.target).after("<div class='pw-hint'><h2>Warning!</h2>You're entering a password on a site which isn't secured by HTTPS (<a href='chrome-extension://ojbegobhciacikcafiomhinpcjhibkgg/src/about-https.html'>?</a>). Others might be able to see your password!</div>");
+	focused = true;
+	$(event.target).after("<div class='pw-hint'><h2>Warning!</h2>You're entering a password on a site which isn't secured by HTTPS (<a href='javascript:void(0)'>?</a>). Others might be able to see your password!</div>");
+	$("div.pw-hint").mouseenter(onMouseOver);
+	$("div.pw-hint").mouseleave(onMouseOut);
+	$("div.pw-hint a").click(function(){
+		window.location.href = chrome.extension.getURL("src/about-https.html");
+	});
 }
 
 function onFieldFBlur(event){
-	$(".pw-hint").remove();
+	focused = false;
+	if(!over){
+		$(".pw-hint").remove();
+	}
+}
+
+function onMouseOver(event){
+	over = true;
+}
+
+function onMouseOut(event){
+	over = false;
+	if(!focused){
+		$(".pw-hint").remove();
+	}
 }
 
 $(document).ready(function(){
